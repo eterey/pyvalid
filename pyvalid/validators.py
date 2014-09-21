c4f0ffa27af18b178b1c83112b0ab502a643b254
uzumaxy <uzumaxy@gmail.com>
2014-09-21 13:54:37 +0300

    Created string validator. Created abstract validator.

---
+++ pyvalid/validators.py
@@ -0,0 +1,74 @@
+import sys
+from abc import ABCMeta, abstractmethod
+from pyvalid import accepts
+from collections import Iterable, Container
+
+
+if sys.version_info >= (3, 0, 0):
+    class Validator(metaclass=ABCMeta):
+        @abstractmethod
+        def __call__(self, val):
+            pass
+else:
+    class Validator(object):
+        __metaclass__ = ABCMeta
+
+        @abstractmethod
+        def __call__(self, val):
+            pass
+
+
+class StringValidator(Validator):
+
+    @classmethod
+    def min_len_checker(cls, val, min_len):
+        return len(val) >= min_len
+
+    @classmethod
+    def max_len_checker(cls, val, max_len):
+        return len(val) <= max_len
+
+    @classmethod
+    def in_range_checker(cls, val, in_range):
+        is_valid = False
+        if isinstance(in_range, Container):
+            is_valid = val in in_range
+        elif isinstance(in_range, Iterable):
+            for item in in_range:
+                if item == val:
+                    is_valid = True
+                    break
+        return is_valid
+
+    @classmethod
+    def not_in_range_checker(cls, val, not_in_range):
+        return not cls.in_range_checker(val, not_in_range)
+
+    _checkers = {
+        'min_len': min_len_checker,
+        'max_len': max_len_checker,
+        'in_range': in_range_checker,
+        'not_in_range': not_in_range_checker
+    }
+
+    @accepts(
+        min_len=int, max_len=int,
+        in_range=[Iterable, Container], not_in_range=[Iterable, Container]
+    )
+    def __init__(self, **kwargs):
+        for key, val in list(kwargs.items()):
+            if (key not in StringValidator._checkers) or (val is None):
+                del kwargs[key]
+        self.__checkers = kwargs
+
+    def __call__(self, val):
+        if isinstance(val, str):
+            valid = True
+            for checker_name, checker_arg in self.__checkers:
+                checker = StringValidator._checkers[checker_name]
+                valid = checker(val, checker_arg)
+                if not valid:
+                    break
+        else:
+            valid = False
+        return valid
