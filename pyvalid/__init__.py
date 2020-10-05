8cf0107f8cfc7f6e231c9893102cda5da69c0f27
Max <uzumaxy@gmail.com>
2020-10-05 03:29:50 +0300

    Organized imports

--- pyvalid/__init__.py
+++ pyvalid/__init__.py
@@ -1,6 +1,7 @@
-from pyvalid.__accepts import Accepts as accepts
-from pyvalid.__returns import Returns as returns
-from pyvalid.__exceptions import PyvalidError, ArgumentValidationError, \
+from .__accepts import Accepts as accepts
+from .__returns import Returns as returns
+from . import validators
+from .__exceptions import PyvalidError, ArgumentValidationError, \
     InvalidArgumentNumberError, InvalidReturnTypeError
 
 version = '1.0.1'
@@ -9,6 +10,7 @@ __all__ = [
     'accepts',
     'returns',
     'switch',
+    'validators',
     'version',
     'PyvalidError',
     'ArgumentValidationError',
