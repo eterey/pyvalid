d61b36811dc80d5858b3c97ccd5f845a30d81d12
Max <uzumaxy@gmail.com>
2020-10-07 05:15:33 +0300

    Typo fixes

--- pyvalid/__init__.py
+++ pyvalid/__init__.py
@@ -1,25 +1,12 @@
-"""The pyvalid is the Python validation tool for checking a function's input
-parameters and return values.
-
-Purposes of the pyvalid package:
-
-#. Provide an ability to validate user input (such as usernames, phone numbers,
-   emails, dates and times, etc) and minimize the amount of code required for
-   the implementation of the comprehensive validation systems;
-#. Add an additional layer of dynamic code analysis for the development and
-   testing stages — pyvalid will raise the exception if a function accepts or
-   returns unexpected values and it's always possible to disable pyvalid in
-   production if needed.
-#. Help to catch runtime issues much easier.
-"""
-
 from pyvalid.__accepts import Accepts as accepts
 from pyvalid.__returns import Returns as returns
 from pyvalid import validators
 from pyvalid.__exceptions import PyvalidError, ArgumentValidationError, \
     InvalidArgumentNumberError, InvalidReturnTypeError
 
-version = '1.0.3'
+
+version = '1.0.4'
+
 
 __all__ = [
     'accepts',
