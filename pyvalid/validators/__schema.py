1631ebb661bf4ce39aa2cc163fbd95e58b8d792d
Max <uzumaxy@gmail.com>
2020-10-25 03:15:56 +0200

    Improved schema validator

--- pyvalid/validators/__schema.py
+++ pyvalid/validators/__schema.py
@@ -3,66 +3,38 @@ from pyvalid.validators import AbstractValidator, Validator
 
 
 class SchemaValidator(AbstractValidator):
-    """Class the validate the input data agains the given schema.
+    r"""Class the validate the input data agains the given schema.
 
         Example:
 
         .. code-block:: python
-            user_schema = SchemaValidator({
-                'name': StringValidator(re_pattern=r'^[A-Za-z]+\s?[A-Za-z]+\s?[A-Za-z]+$'),
-                'birthyear': NumberValidator(min_val=1990, max_val=2010),
-                'rating': float,
-                # 'bio': [StringValidator(max_len=2048), None]
-            })
 
-            new_schema = SchemaValidator({
-                'name': StringValidator(re_pattern=r'^[A-Za-z]+\s?[A-Za-z]+\s?[A-Za-z]+$'),
+            user_schema = SchemaValidator({
+                'name': StringValidator(
+                    re_pattern=r'^[A-Za-z]+\s?[A-Za-z]+\s?[A-Za-z]+$'
+                ),
                 'birthyear': NumberValidator(min_val=1890, max_val=2020),
                 'rating': float,
-                'bio': StringValidator(min_len=1),
-                'new_data': user_schema
+                'bio': [StringValidator(max_len=2048), None]
             })
 
 
             # So we can use schemas for `@accepts` and `@returns`
-            # @accepts(new_user=user_schema)
-            # def register_user(new_user):
-            #     # some logic here
-            #     pass
+            @accepts(new_user=user_schema)
+            def register_user(new_user):
+                 # some logic here
+                 pass
 
 
             # Or we can call them directly to validate some data
-            # user_schema({
-            #     'name': 'Max',
-            #     'birthyear': -42,  # will cause a validator error
-            #     'bio': None,
-            #     'rating': 8.0
-            #     # the `address` attribute is missing, what will cause another error even if we fix `birthyear`
-            # })
-
-            old_user = {
-                'name': 'Nagesh',
-                'birthyear': 1994,  # will cause a validator error
-                # 'bio': None,
-                'rating': 8.0
-            }
-
-            @accepts(new_user=new_schema, X=StringValidator(re_pattern=r'^[A-Za-z]+\s?[A-Za-z]+\s?[A-Za-z]+$'))
-            def register_users(new_user, X):
-                # some logic here
-                pass
-
-            new_user = {
-                'name': 'Nandeesh',
-                'birthyear': 2020,  # will cause a validator error
-                'bio': "Nagesh",
-                'rating': 8.0,
-                'new_data': old_user
-                # the `address` attribute is missing, what will cause another error even if we fix `birthyear`
-            }
-
-            register_users(new_user, "Nandan")
-
+            user_schema({
+                 'name': 'Nandan',
+                 'birthyear': -42,  # will cause a validator error
+                 'bio': None,
+                 'rating': 8.0
+                 # the `address` attribute is missing, what will cause another error
+                 # even if we fix the `birthyear`
+            })
     """
 
     DEFAULT_TYPES = (float, int, complex, dict, bool, set, str, list, tuple)
@@ -90,12 +62,7 @@ class SchemaValidator(AbstractValidator):
             SchemaValidator.required_keys: [schema],
             SchemaValidator.schema_compliance: [schema]
         }
-        AbstractValidator.__init__(self)
-
-    @accepts(object, dict)
-    def __call__(self, val):
-        valid = self._check(val)
-        return valid
+        AbstractValidator.__init__(self, allowed_types=dict)
 
     @property
     def checkers(self):
--- tests/test_schema_validator.py
+++ tests/test_schema_validator.py
@@ -6,12 +6,11 @@ from pyvalid.validators import SchemaValidator, StringValidator, NumberValidator
 class SchemaValidatorTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.new_user = {
+        self.user = {
             'rating': 8.0,
             'name': 'Max',
-            'birthyear': 1994,  # will cause a validator error
+            'birthyear': 1994,
             'bio': None
-            # the `address` attribute is missing, what will cause another error even if we fix `birthyear`
         }
 
     def test_schema(self):
@@ -21,11 +20,12 @@ class SchemaValidatorTestCase(unittest.TestCase):
             'rating': float,
             'bio': [StringValidator(max_len=1024), None]
         })
-        self.assertTrue(schema(self.new_user))
-        self.new_user['rating'] = 8
-        self.assertFalse(schema(self.new_user))
-        self.new_user.__delitem__('rating')
-        self.assertFalse(schema(self.new_user))
+        self.assertTrue(schema(self.user))
+        self.user['rating'] = 8
+        self.assertFalse(schema(self.user))
+        self.user.__delitem__('rating')
+        self.assertFalse(schema(self.user))
+        self.assertFalse(schema(None))
 
     def test_schema_input(self):
         schema = SchemaValidator({
@@ -43,12 +43,12 @@ class SchemaValidatorTestCase(unittest.TestCase):
         default_user = {
             'name': 'Max',
             'birthyear': 1994,  # will cause a validator error
-            'manager': self.new_user
+            'manager': self.user
         }
         self.assertTrue(sub_schema(default_user))
-        self.new_user['rating'] = 8
+        self.user['rating'] = 8
         self.assertFalse(sub_schema(default_user))
-        self.new_user.__delitem__('rating')
+        self.user.__delitem__('rating')
         self.assertFalse(sub_schema(default_user))
 
 
